0664f5edf6f27c96e56eba23afb6e7dfdb9a2898
Juan Murcia <murcianieto.juanesteban@gmail.com>
2021-10-23 22:16:27 -0500

    Añadido test para rutas de prestamos

---
+++ test/servicios/API/routers/prestamos.spec.js
@@ -0,0 +1,7 @@
+var assert = require('assert');
+
+describe("Deberian funcionar todas las rutas de prestamos", async () => {
+    it("Deberia poderse eliminar un prestamo", async () => {
+        assert.equal(1,1)
+    })
+})
