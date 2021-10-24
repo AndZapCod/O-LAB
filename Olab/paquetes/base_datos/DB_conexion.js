4ec2e8cb4483807ed073097eac2da7f7f362b4c1
Juan Murcia <murcianieto.juanesteban@gmail.com>
2021-10-23 19:19:36 -0500

    Añadido un timeout a la conexion con la DB

--- Olab/paquetes/base_datos/DB_conexion.js
+++ Olab/paquetes/base_datos/DB_conexion.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
+
 const Pool= require('pg').Pool;
 const pool= new  Pool({
     user: 'postgres',
-    host: 'localhost',
+    host: process.env['PSQL_HOST'],
     database: 'olab',
-    password: 'postgres',
-    port: 5432
+    password: process.env['PSQL_PASS'],
+    port: 5432,
+    connectionTimeoutMillis : 5000
 });
 
 module.exports=pool;
